f1e9014049ed986513afa7ef74e483160ac9c0cf
IT20273408 <it20273408@my.sliit.lk>
2021-12-01 18:33:55 +0530

    init

---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html> 
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scarle=1.0" >
+
+    <link rel = "stylesheet" href= "https://stackpath.boostrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+
+    <link rel = "stylesheet" type= "text/css" href= "style.css">
+
+    <title> Login Form </title>
+
+</head> 
+
+<body>
+    <div class="container">
+        <form class= "login-email">
+            <p class= "login-text" style="font-size: 2rem; font-weight: 800;"> Login </p>
+            <div class=input-group>
+                <input type="email" placeholder= "Email"  required >
+                
+</div>
+
+<div class="input-group">
+    <input type="password" placeholder="Password" requird>
+</div>
+
+<div class = "input-group">
+    <button class ="btn"> Login </button> 
+    
+</div>
+
+</form>
+</div>
+
+<p class= "login-register-text"> Don't have an account? <a href="register.php">Register Here</a>.</p>
+
+</body>
+</html>
---
+++ register.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html> 
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scarle=1.0" >
+
+    <link rel = "stylesheet" href= "https://stackpath.boostrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+
+    <link rel = "stylesheet" type= "text/css" href= "style.css">
+
+    <title> Register Form </title>
+
+</head> 
+
+<body>
+    <div class="container">
+        <form class= "login-email">
+            <p class= "login-text" style="font-size: 2rem; font-weight: 800;"> Register </p>
+            <div class=input-group>
+                <input type="text" placeholder= "Username" name="username"  required >
+                
+</div>
+<div class=input-group>
+                <input type="email" placeholder= "Email" name= "email" required >
+                
+</div>
+
+
+<div class="input-group">
+    <input type="password" placeholder="Password" name= "password" requird>
+</div>
+<div class="input-group">
+    <input type="password" placeholder="Confirm Password" name ="cpassword" requird>
+</div>
+
+
+<div class = "input-group">
+    <button class ="btn"> Register </button> 
+    
+</div>
+
+</form>
+</div>
+
+<p class= "login-register-text">  Have an account? <a href="index.php">Login Here</a>.</p>
+
+</body>
+</html>
